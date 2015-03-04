891a40b74638aabb0535c30765625129f9ffa3b6
Mo Omer <beancinematics@gmail.com>
2015-03-03 22:27:28 -0600

    More improvements; changed dictionary to typedef which should work for all instances; hurray

--- haxelib/Dictionary.hx
+++ haxelib/Dictionary.hx
@@ -2,5 +2,5 @@ package ;
 
 
 @:native("Dictionary")
-extern class Dictionary
-{}
+@:generic
+typedef Dictionary<T> = Map<String, T>;
--- test/test/TestFile.hx
+++ test/test/TestFile.hx
@@ -1,4 +1,4 @@
-package Test;
+package test;
 
 import global.Console;
 import global.JSON;
